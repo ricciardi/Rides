ded8f1a4cdb573ce3ddd2763950222dcc9967758
Logan McCamon <logan@mccamon.org>
2013-07-14 16:19:56 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,22 +1,21 @@
-/*DROP DATABASE Rides;
-CREATE DATABASE Rides;
-USE Rides;
+/*DROP DATABASE rides;
+CREATE DATABASE rides;
+USE rides;
 */
 
 CREATE TABLE IF NOT EXISTS User(
 P_Id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
 FB_Id bigint UNSIGNED NOT NULL,
 Name varchar(32),
+Phone int(10) DEFAULT NULL,
 PRIMARY KEY (`P_Id`),
 CONSTRAINT UNIQUE KEY `FB_Id` (`FB_Id`));
-
 
 CREATE TABLE IF NOT EXISTS Groups(
 Group_Id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
 Name varchar(32) NOT NULL,
 Created datetime NOT NULL,
 PRIMARY KEY (`Group_Id`)); 
-
 
 CREATE TABLE IF NOT EXISTS Admin(
 P_Id bigint UNSIGNED NOT NULL,
@@ -62,3 +61,28 @@ FOREIGN KEY (From_Id) REFERENCES User(P_Id),
 FOREIGN KEY (To_Id) REFERENCES User(P_Id),
 FOREIGN KEY (Group_Id) REFERENCES Groups(Group_Id),
 FOREIGN KEY (Response_Status) REFERENCES Invite_Status(Invite_Status_Id));
+
+INSERT INTO User(FB_Id, Name, Phone)
+VALUES   	(120384, 'Bob Smith', 3332221111),
+			(238418, 'John Smith', 1112223333),
+			(2039, 'Sue Flann', 3939393939),
+			(3939, 'John Jones', 3932023030);
+
+INSERT INTO Groups(Name, Created)
+VALUES		('Family', '2009-04-15 14:00:00'),
+			('Friends', '2010-03-15 12:00:00'),
+			('School', '2012-07-16 15:00:00');
+
+INSERT INTO Admin(P_Id, Group_Id)
+VALUES		(1,1),
+			(1,2),
+			(2,3);
+
+INSERT INTO Member(P_Id,Group_Id,Joined)
+VALUES		(1,1,'2009-04-15 14:00:01'),
+			(1,2,'2010-03-15 12:00:01'),
+			(2,3,'2012-07-16 15:00:01'),
+			(3,2,'2010-03-16 12:00:03'),
+			(4,2,'2010-03-18 12:00:05'),
+			(2,1,'2009-04-16 14:00:00');
+
